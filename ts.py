4761a7f00a74d503f819fa81a75df00b8bbb30ef
Calvin-T <tang.calvin1@gmail.com>
2019-02-15 13:03:46 -0500

    Implemented dictionary and populated the DNS_Table for both RS and TS

---
+++ ts.py
@@ -0,0 +1,15 @@
+import socket
+
+
+TS_DNS_Table = {}
+
+def populateData(filename):
+	lines = [line.rstrip('\r\n') for line in open(filename)]
+	for line in lines:
+		lineSplit = line.split()
+		TS_DNS_Table[lineSplit[0]] = [lineSplit[1],lineSplit[2]]
+		
+	print(TS_DNS_Table)
+
+if __name__ == '__main__':
+	populateData("PROJI-DNSTS.txt")
