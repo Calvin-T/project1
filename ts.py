ce817ebbaf48a20de78460e6e2135a866255eb9c
csyang98 <43451330+csyang98@users.noreply.github.com>
2019-02-17 10:24:57 -0500

    added server socket and command line args

--- ts.py
+++ ts.py
@@ -1,6 +1,22 @@
 import socket
 
 
+import sys
+"""Top level name server program"""
+
+tsListenPort=0
+if len(sys.argv) == 2:
+    # check that this is an integer, make sure that it is non-neg
+    if sys.argv[1].isdigit():
+        tsListenPort = int(sys.argv[1])
+    else:
+        # error, invalid port number and exit program
+        exit(1)
+else:
+    # error, too many or too few command line args-> exit()
+    exit(1)
+
+
 TS_DNS_Table = {}
 
 def populateData(filename):
@@ -13,3 +29,29 @@ def populateData(filename):
 
 if __name__ == '__main__':
 	populateData("PROJI-DNSTS.txt")
+	
+	
+try:
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("[S]: Server socket created")
+except socket.error as err:
+    print('socket open error: {}\n'.format(err))
+    exit()
+
+server_binding = ('', tsListenPort)
+serverSocket.bind(server_binding)
+serverSocket.listen(1)
+host = socket.gethostname()
+print("[S]: Server host name is {}".format(host))
+localhost_ip = (socket.gethostbyname(host))
+print("[S]: Server IP address is {}".format(localhost_ip))
+csockid, addr = serverSocket.accept()
+print ("[S]: Got a connection request from a client at {}".format(addr))
+
+# send a intro message to the client.
+msg = "Connected to TS server!"
+csockid.send(msg.encode('utf-8'))
+
+# Close the server socket
+serverSocket.close()
+exit()
