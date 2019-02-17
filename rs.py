e6a85c2046f35684087dc5e116c7910a92e5c649
csyang98 <43451330+csyang98@users.noreply.github.com>
2019-02-17 10:23:22 -0500

    added server socket code

--- rs.py
+++ rs.py
@@ -1,5 +1,7 @@
 import socket
+import sys
 
+"""Root level name server program"""
 
 RS_DNS_Table = {}
 
@@ -13,3 +15,38 @@ def populateData(filename):
 
 if __name__ == '__main__':
 	populateData("PROJI-DNSRS.txt")
+	
+
+rsListenPort = 0
+
+if len(sys.argv) == 2:
+    #check that this is an integer, make sure that it is non-neg
+    if sys.argv[1].isdigit():
+        rsListenPort = int(sys.argv[1])
+    else:
+        # error, invalid port number and exit program
+        exit(1)
+else:
+    # error, too many or too few command line args-> exit()
+    exit(1)
+
+server_binding = ('', rsListenPort) #50007
+serverSocket.bind(server_binding)
+serverSocket.listen(1)
+host = socket.gethostname()
+print("[S]: Server host name is {}".format(host))
+localhost_ip = (socket.gethostbyname(host))
+print("[S]: Server IP address is {}".format(localhost_ip))
+csockid, addr = serverSocket.accept()
+print("[S]: Got a connection request from a client at {}".format(addr))
+
+# send a intro message to the client.
+msg = "Connected to RS server"
+csockid.send(msg.encode('utf-8'))
+#then wait for queries
+#have a closing protocol
+#client finishes, send an exit message to server, then server will call close()
+
+# Close the server socket
+serverSocket.close()
+exit()
